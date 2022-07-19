1075a270d891a77a1db0f43d5c14b68288cfea7c
Dinesh Bodala <ds176.edu@gmail.com>
2022-07-19 20:48:12 +0530

    Adding source code 1.0

---
+++ cryptowatchlist/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CryptowatchlistConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cryptowatchlist'
---
+++ cryptowatchlist/views.py
@@ -0,0 +1,26 @@
+from curses.ascii import HT
+from operator import index
+import re
+from django.shortcuts import render
+from django.http import HttpResponse
+import ccxt
+from datetime import datetime
+import cryptowatchlist
+import datetime
+from django.http import HttpResponseRedirect
+from django.forms import Form
+
+def index(request):
+    ct =str(datetime.datetime.now().replace(second=0, microsecond=0))
+    exchange = ccxt.binance()
+    added_time = int(datetime.datetime.strptime("2022-06-27 09:00:00", "%Y-%m-%d %H:%M:%S").timestamp() * 1000)
+    current_time=int(datetime.datetime.strptime(ct, "%Y-%m-%d %H:%M:%S").timestamp() * 1000)
+    price_current_time = exchange.fetch_ohlcv('BTC/USDT', '1m',current_time, 1)
+    price_added_time = exchange.fetch_ohlcv('BTC/USDT', '1m', added_time, 1)
+
+
+    total=int(price_added_time[0][4])-int(price_current_time[0][4])
+
+
+
+    return HttpResponse(total)
