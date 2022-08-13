733c29cdf98dc1e7685fc4c6e266840806641691
Dinesh Bodala <ds176.edu@gmail.com>
2022-08-13 23:17:39 +0530

    Adding Styling

--- cryptowatchlist/views.py
+++ cryptowatchlist/views.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 from multiprocessing import context
 from operator import index
 import re
@@ -20,14 +20,16 @@ exchange = ccxt.binance()
 def index(request):
     users=User.objects.get(id=3)
     y=[]
+    c=0
     coins=users.crypto_set.all()
-    coin_count=coins.count()
     ct = str(datetime.datetime.now().replace(second=0, microsecond=0))
     for i in coins:
-        added_time = int(datetime.datetime.strptime("2022-06-27 09:00:00", "%Y-%m-%d %H:%M:%S").timestamp() * 1000)
+        added_time = str(i.added_time.replace(second=0, microsecond=0))
+        added_time=added_time[:19]
+        c_added_time=int(datetime.datetime.strptime(added_time, "%Y-%m-%d %H:%M:%S").timestamp() * 1000)
         current_time = int(datetime.datetime.strptime(ct, "%Y-%m-%d %H:%M:%S").timestamp() * 1000)
         price_current_time = exchange.fetch_ohlcv(i.crypto_name+'/USDT', '1m', current_time, 1)
-        price_added_time = exchange.fetch_ohlcv(i.crypto_name+'/USDT', '1m', added_time, 1)
+        price_added_time = exchange.fetch_ohlcv(i.crypto_name+'/USDT', '1m', c_added_time, 1)
         total = int(price_added_time[0][4]) - int(price_current_time[0][4])
         y.append(total)
 
